53547d0b8c4d00f32f9cb6c908c5e8f9b4d77580
Admin <Admin@DESKTOP-VT4B3DF>
2024-11-09 11:07:36 +0700

    Add project files.

---
+++ DemoReport/Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoReport
+{
+    internal class Class1
+    {
+        private string connectionString = @"Data Source=DESKTOP-VT4B3DF\SQLEXPRESS;Initial Catalog=TranhAnh;User ID=sa;Password=abc123";
+
+
+        public DataTable GetThuMuc()
+        {
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                string query = "Select * from HangHoa";
+                SqlDataAdapter adt = new SqlDataAdapter(query,conn);
+                DataTable dt = new DataTable();
+                adt.Fill(dt);
+                return dt;
+            }
+        }
+    }
+}
---
+++ DemoReport/Form1.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Reporting.WinForms;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DemoReport
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Class1 data = new Class1();
+            reportViewer1.LocalReport.ReportEmbeddedResource = "DemoReport.Report1.rdlc";
+            ReportDataSource reportData = new ReportDataSource();
+            reportData.Name = "DataSet1";
+            reportData.Value = data.GetThuMuc();
+            reportViewer1.LocalReport.DataSources.Add(reportData);
+            this.reportViewer1.RefreshReport();
+        }
+    }
+}
